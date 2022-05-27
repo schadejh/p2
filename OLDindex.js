be90887673eda41aa35ec526dcd6d9376c9adb8b
James H. Schader <schadejh@dukes.jmu.edu>
2022-05-27 14:08:59 -0400

    adding update and delete methods and renaming files

---
+++ OLDindex.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const mysql = require('mysql');
+
+const json = fs.readFileSync('credentials.json', 'utf8');
+const credentials = JSON.parse(json);
+
+const connection = mysql.createConnection(credentials);
+connection.connect(error => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
+
+// TODO: issue queries.
+/*  -- EXAMPLE OF AN INSERT QUERY 
+const insertQuery = 'INSERT INTO vehicles(id, regNo, class, location) VALUES (?, ?, ?, ?)';
+const parameters = [666, '420', 'M14', 'Hell'];
+connection.query(insertQuery, parameters, (error, result) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(result);
+  }
+});
+*/
+
+function rowToMemory(row) {
+  return {
+    registry: row.regNo + ' an ' + row.class + ' at ' + row.location,
+    lastMove: row.dateLastMoved,
+  };
+}
+
+connection.query('SELECT * FROM vehicles', (error, rows) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const vehicles = rows.map(rowToMemory);
+    console.log(vehicles);
+  }
+});
+
+connection.end();
+
+// token: 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
--- index.js
+++ index.js
@@ -1,46 +1,132 @@
+const express = require('express');
 const fs = require('fs');
 const mysql = require('mysql');
 
 const json = fs.readFileSync('credentials.json', 'utf8');
 const credentials = JSON.parse(json);
-
 const connection = mysql.createConnection(credentials);
+
 connection.connect(error => {
-  if (error) {
-    console.error(error);
-    process.exit(1);
-  }
+	if (error) {
+		console.error(error);
+		process.exit(1);
+	}
 });
 
-// TODO: issue queries.
-/*  -- EXAMPLE OF AN INSERT QUERY 
-const insertQuery = 'INSERT INTO vehicles(id, regNo, class, location) VALUES (?, ?, ?, ?)';
-const parameters = [666, '420', 'M14', 'Hell'];
-connection.query(insertQuery, parameters, (error, result) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(result);
-  }
-});
-*/
-
+// helper function for showing a row's info on the console
 function rowToMemory(row) {
-  return {
-    registry: row.regNo + ' an ' + row.class + ' at ' + row.location,
-    lastMove: row.dateLastMoved,
-  };
+	return {
+		registry: row.regNo + ' an ' + row.class + ' at ' + row.location,
+		lastMove: row.dateLastMoved,
+	};
 }
 
-connection.query('SELECT * FROM vehicles', (error, rows) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const vehicles = rows.map(rowToMemory);
-    console.log(vehicles);
-  }
+function create(row) {
+	const insertQuery = 'INSERT INTO Vehicles(regNo, class, location, dateLastMoved, deadlined) VALUES (?, ?, ?, ?, ?)';
+	// get parameters from user
+	connection.query(insertQuery, parameters, (error, result) => {
+		if (error) {
+			console.error(error);
+		} else {
+			const vehicle = row.map(rowToMemory);
+			console.log(result);
+		}
+	});
+}
+
+// function readAll() {
+// 	connection.query('SELECT * FROM Vehicles', (error, rows) => {
+// 		if (error) {
+// 			console.error(error);
+// 		} else {
+// 			const vehicles = rows.map(rowToMemory);
+// 			console.log(vehicles);
+// 		}
+// 	});
+// }
+
+function readOne(id) {
+	connection.query('SELECT * FROM Vehicles WHERE id=?', (error, row) => {
+		if (error) {
+			console.error(error);
+		} else {
+			const vehicle = row.map(rowToMemory); // make a full row to memory function
+			console.log(vehicle);
+		}
+	});
+}
+
+function update(row) {
+	connection.query('UPDATE Vehicles SET regNo=?, class=?, location=?, dateLastMoved=?, deadlined=? WHERE id=?', (error, row) => {
+		if (error) {
+			console.error(error);
+		} else {
+			const vehicle = row.map(rowToMemory);
+			console.log(row);
+		}
+	});
+}
+
+// function deleteRow(row) {
+// 	connection.query('UPDATE Vehicles SET deleted=1 WHERE id=?', (error, row) => {
+// 		if (error) {
+// 			console.error(error);
+// 		} else {
+// 			const vehicle = row.map(rowToMemory);
+// 			console.log(row);
+// 		}
+// 	});
+// }
+
+// define endpoints...
+const service = express();
+
+service.get('/vehicles/', (request, response) => {
+	connection.query('SELECT * FROM Vehicles WHERE deleted=false', (error, rows) => {
+		if (error) {
+			console.error(error);
+			response.status(500);
+			response.json({
+				ok: false,
+				results: error.message,
+			});
+		} else {
+			const vehicles = rows.map(rowToMemory);
+			console.log(vehicles);
+			response.json({
+				ok: true,
+				results: vehicles,
+			});
+		}
+	});
+
 });
 
-connection.end();
+// 'DELETE' ENDPOINT
+service.delete('/vehicles/:id', (request, response) => {
+	const parameters = [parseInt(request.params.id)];
+		connection.query('UPDATE Vehicles SET deleted=TRUE WHERE id=?', (error, rows) => {
+			if (error) {
+				console.error(error);
+				response.status(500);
+				response.json({
+					ok: false,
+					results: error.message,
+				});
+			} else {
+				const vehicles = rows.map(rowToMemory);
+				console.log(vehicles);
+				response.json({
+					ok: true,
+					results: vehicles,
+				});
+			}
+	});
+});
 
-// token: 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
+const port = 5001;
+service.listen(port, () => {
+	console.log(`We're live in port ${port}!`);
+});
+
+// connection.end();
